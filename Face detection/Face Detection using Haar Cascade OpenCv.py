6cde0a360ee73bb8cf06bdd19b67b81183081407
AasaiAlangaram <43054456+AasaiAlangaram@users.noreply.github.com>
2020-01-27 21:15:50 +0900

    code updated

--- Face detection/Face Detection using Haar Cascade OpenCv.py
+++ Face detection/Face Detection using Haar Cascade OpenCv.py
@@ -14,15 +14,16 @@ while True:
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.2, 1)
+    faces = face_cascade.detectMultiScale(gray, 1.2, 2)
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
     # Display
     cv2.imshow('img', img)
-    # Stop if escape key is pressed
-    k = cv2.waitKey(30) & 0xff
-    if k==27:
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-# Release the VideoCapture object
+
+# When everything done, release the capture
 cap.release()
+cv2.destroyAllWindows()
