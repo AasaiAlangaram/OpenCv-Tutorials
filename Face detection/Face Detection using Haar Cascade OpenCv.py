39c85162218aed1e2b3a1d3295e727718828cf6a
AasaiAlangaram <43054456+AasaiAlangaram@users.noreply.github.com>
2020-01-27 21:38:50 +0900

    cv2.rectangle commented

--- Face detection/Face Detection using Haar Cascade OpenCv.py
+++ Face detection/Face Detection using Haar Cascade OpenCv.py
@@ -17,10 +17,12 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.2, 2)
     # Draw the rectangle around each face
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display
+        #  cv2.rectangle(image, start_point, end_point, color(B,G,R), thickness)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Display image
     cv2.imshow('img', img)
 
+    # close once q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
