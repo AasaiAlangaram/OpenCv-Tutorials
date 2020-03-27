38341c5fb9013f6faee528e4847f77bf6cc95148
AasaiAlangaram <43054456+AasaiAlangaram@users.noreply.github.com>
2020-03-27 19:44:55 +0900

    Add files via upload

--- Face detection/Face Detection using Haar Cascade OpenCv.py
+++ Face detection/Face Detection using Haar Cascade OpenCv.py
@@ -1,31 +1,29 @@
 import cv2
 
-# Load the cascade
+# To use a image file as input
+img_path = 'C:\\Users\\aasai\\Desktop\\TheHobbyists - Opencv Tutorials\\27-03-2020\\aasai.jpeg'
+
+# load our image and convert it to grayscale
+image = cv2.imread(img_path)
+
+print(image.shape)
+image = cv2.resize(image,(450,600))
+# print('row,column,no.of.channels',image.shape)
+
+#convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# load the face detector and detect faces in the image
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# It capture video from computer camera.
-cap = cv2.VideoCapture(0)
-# To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
+rects = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=8)
+print(rects)
+print("[INFO] detected {} faces".format(len(rects)))
 
-while True:
-    # Read the frame
-    ret, img = cap.read()
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.2, 2)
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        #  cv2.rectangle(image, start_point, end_point, color(B,G,R), thickness)
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    # Display image
-    cv2.imshow('img', img)
+# loop over the bounding boxes and draw a rectangle around each face
+for (x, y, w, h) in rects:
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0,255,0), 2)
 
-    # close once q is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+# show the detected faces
+cv2.imshow("Faces", image)
+cv2.waitKey(0)
